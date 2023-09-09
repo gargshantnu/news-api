9feba54d8250ac918ecd50c9af9d8b89da932c95
Shantnu Garg <gargshantnu@gmail.com>
2023-09-09 17:33:50 +0530

    add support to cache trending articles

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,14 +1,33 @@
 const config = require("../config/config");
 const gNewsUtil = require("../utils/GNews.util");
-
+const hazelCacheClient = require("../utils/hazelCache");
 
 const getTrendingNewsArticles = async (req, resp, next) => {
     try {
         // top n articles
         let { limit } = req.query;
-        let r = await gNewsUtil.getTrendingNews(limit);
-        console.log(r.data);
-        return resp.json(r.data);
+        let data = await hazelCacheClient.getDataFromMap(
+            "TRENDING_NEWS_LIST",
+            "a"
+        );
+        let articleList;
+        
+        if (!data || JSON.parse(data).length < limit) {
+            // if there is no data in cache or cached data size is low - make new api call and cache that data.
+            console.log("calling api", limit);
+            articleList = await gNewsUtil.getTrendingNews(limit);
+            articleList = articleList.data.articles;
+            hazelCacheClient.setDataIntoMap(
+                "TRENDING_NEWS_LIST",
+                "a",
+                JSON.stringify(articleList)
+            );
+        } else {
+            console.log("returning cached data", limit);
+            articleList = JSON.parse(data).splice(0, limit);
+        }
+
+        return resp.json(articleList);
     } catch (e) {
         console.log(e);
     }
@@ -18,9 +37,8 @@ const searchNewsArticles = async (req, resp, next) => {
     try {
         // by title or description or contents
         let { title, description, content } = req.query;
-        let r = await gNewsUtil.searchNews(title, description, content);
-        console.log(r.data);
-        return resp.json(r.data);
+        let articleList = await gNewsUtil.searchNews(title, description, content);
+        return resp.json(articleList.data.articles);
     } catch (e) {
         console.log(e);
     }
